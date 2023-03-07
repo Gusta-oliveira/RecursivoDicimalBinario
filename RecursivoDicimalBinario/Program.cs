5d38c1cd395a85b8a7f9523c031d93fcc4bbac34
Gustavo Oliveira <gustavo.ramosue@gmail.com>
2023-03-07 02:14:37 -0300

    Add project files.

---
+++ RecursivoDicimalBinario/Program.cs
@@ -0,0 +1,22 @@
+﻿int numero, ctrl = 7;
+int[] bin = new int[8];
+Console.Write("Informe o valor para converter em binário: ");
+numero = int.Parse(Console.ReadLine());
+
+binario(numero);
+
+for (ctrl = 0; ctrl < 8; ctrl++)
+{
+    Console.Write(bin[ctrl]);
+}
+int binario(int numero)
+{
+    if (numero > 0)
+    {
+        bin[ctrl] = numero % 2;
+        ctrl--;
+        return binario(numero / 2);
+    }
+    return 0;
+}
+
